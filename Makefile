a25f9ed3f2099eefa4dd603eeed0867d76a1d984
Victor Pereira <vpereira@suse.de>
2015-02-15 14:35:49 +0100

    added dependencies to Makefile and added sessions to login

--- Makefile
+++ Makefile
@@ -10,6 +10,7 @@ deps:
 	go get github.com/go-martini/martini
 	go get github.com/martini-contrib/binding
 	go get github.com/martini-contrib/render
+	go get github.com/martini-contrib/sessions
 
 build:
 	cd services/login && go build -o login
--- services/login/login.go
+++ services/login/login.go
@@ -2,11 +2,14 @@ package main
 
 import (
   "fmt"
+  "net/http"
+  "github.com/jinzhu/gorm"
   "github.com/vpereira/micro-topt/libs/utils"
   "github.com/vpereira/micro-topt/models"
   "github.com/go-martini/martini"
   "github.com/martini-contrib/binding"
   "github.com/martini-contrib/render"
+  "github.com/martini-contrib/sessions"
   )
 
 // Binding from JSON
@@ -14,23 +17,51 @@ type LoginJSON struct {
     User     string `json:"user" binding:"required"`
     Password string `json:"password" binding:"required"`
 }
+
+func RequireLogin(rw http.ResponseWriter, req *http.Request,
+  s sessions.Session, db *gorm.DB, c martini.Context) {
+    user := model.User{}
+
+    v := s.Get("userId")
+    if v != nil {
+      iId := v.(int64)
+
+      if db.Where(&model.User{Id: iId}).First(&user).RecordNotFound() {
+        // user not found
+        http.Redirect(rw,req,"/login",http.StatusFound)
+      }
+      c.Map(user)
+    }
+
+  }
 func main() {
     m := martini.Classic()
     user := model.User{}
     m.Use(render.Renderer())
+    // Sessions
+    store := sessions.NewCookieStore([]byte("0xd34db33f"))
+    m.Use(sessions.Sessions("auth",store))
+    // Connect with the db. Today we are using sqlite3. In the future, postgres
     db,_ := utils.SetupDB("sqlite3", fmt.Sprintf("%s/users.db","db"))
     defer db.Close()
     // to test it:
-    // curl -H "Content-Type: application/json"  -X POST -d '{"user":"foo","password":"123"}'  http://localhost:8080/login
-    m.Post("/login", binding.Json(LoginJSON{}), binding.ErrorHandler, func(json LoginJSON,r render.Render) {
+    // curl -H "Content-Type: application/json" \
+    // -X POST -d '{"user":"foobar","password":"foobarmar"}' \
+    // http://localhost:8080/login
+    m.Post("/login", binding.Json(LoginJSON{}), binding.ErrorHandler, func(json LoginJSON,r render.Render,s sessions.Session) {
       // By this point, I assume that my own middleware took care of any errors
       pwd := utils.GetHash(json.Password)
       if db.Where(&model.User{Login: json.User, Password: pwd}).First(&user).RecordNotFound() {
         r.JSON(401,map[string]interface{}{"bad": "world"})
       } else {
         r.JSON(200,map[string]interface{}{"hello": "world"})
+        s.Set("userId",user.Id)
       }
     })
-    // Listen and server on 0.0.0.0:8080
+    m.Get("/",RequireLogin,func(u *model.User) string {
+       return "Hello: " + u.Login
+      })
+
+    // Listen and serve it
     m.Run()
 }
